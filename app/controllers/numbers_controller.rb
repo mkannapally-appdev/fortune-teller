93e2e04e50d4baf66646ef5a734c2ce743cebc3c
mkannapally <akannapa@chicagobooth.edu>
2022-10-20 19:25:22 +0000

    unlucky working well

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,17 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+     @unlucky_numbers = Array.new
+     
+     5.times do
+      random_number = rand(1...100)
+      
+      @unlucky_numbers.push(random_number)
+     end
+
+    render({:template => "lottery_stuff/unlucky_numbers.html.erb"})
+
+  end
 end
